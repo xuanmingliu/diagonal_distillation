399a761e81207d94bf54281088a802afb6a94562
xuanmingliu <xuanmingliu0120@gmail.com>
2025-10-01 01:04:09 +0800

    Delete inference.sh

--- inference.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=7
-python inference.py \
-    --config_path configs/self_forcing_dmd.yaml \
-    --output_folder videos/self_forcing_dmd \
-    --checkpoint_path checkpoints/self_forcing_dmd.pt \
-    --data_path prompts/MovieGenVideoBench_extended.txt \
-    --use_ema 
